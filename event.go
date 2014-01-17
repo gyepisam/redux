143e3ae89522e110f51a00e957efd9ea702dc190
Gyepi Sam <self@gyepi.com>
2014-01-17 11:39:38 +0200

    wip

---
+++ event.go
@@ -0,0 +1,24 @@
+package redo
+
+/*
+* An Event denotes a state change upon which dependencies are based.
+* The line "redo-ifchange B" in the file A.do creates a prerequisite
+* from A to B based on the ifchange event.
+ */
+
+// Event names a change or create event
+type Event string
+
+// Events of note
+const (
+	IFCREATE Event = "ifcreate"
+	IFCHANGE Event = "ifchange"
+
+	AUTO_IFCREATE Event = AUTO + KEY_SEPARATOR + "ifcreate"
+	AUTO_IFCHANGE Event = AUTO + KEY_SEPARATOR + "ifchange"
+)
+
+// String representation.
+func (event Event) String() string {
+	return string(event)
+}
