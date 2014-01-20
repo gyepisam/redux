f302fa93702f0d95185bf4544c66c8159ccba8a3
Gyepi Sam <self@gyepi.com>
2014-01-20 12:47:26 +0200

    gofmt

--- common.go
+++ common.go
@@ -39,8 +39,9 @@ const (
 // Directory creation permission mode
 const DIR_PERM = 0755
 
-// makeKey returns a database key consisting of provided arguments, prefixed
-// with the path hash.
+
+// makeKey returns a database key consisting of provided arguments, joined with KEY_SEPARATOR
+// and prefixed with the PathHash.
 func (f *File) makeKey(subkeys ...interface{}) (val string) {
 
 	keys := make([]string, len(subkeys)+1)
