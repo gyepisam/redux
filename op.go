aa286d99275c11e8096872685b1ecbc628244923
Gyepi Sam <self@gyepi.com>
2014-01-20 12:48:35 +0200

    moved InitDir into init.go

--- op.go
+++ op.go
@@ -416,22 +416,4 @@ func (target *File) RedoIfCreate(dependent *File) error {
 	return RecordRelation(dependent, target, IFCREATE, nil)
 }
 
-// InitDir initializes a redo directory in the specified project root directory, creating it if necessary.
-func InitDir(dirname string) error {
 
-	if len(dirname) == 0 {
-		wd, err := os.Getwd()
-		if err != nil {
-			return err
-		}
-		dirname = wd
-	} else if c := dirname[0]; c != '.' && c != '/' {
-		wd, err := os.Getwd()
-		if err != nil {
-			return err
-		}
-		dirname = filepath.Join(wd, dirname)
-	}
-
-	return os.MkdirAll(filepath.Join(dirname, REDO_DIR), DIR_PERM)
-}
