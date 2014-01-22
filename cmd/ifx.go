5d263e3ad2c104ba6556f14fe5922fedae84f998
Gyepi Sam <self@gyepi.com>
2014-01-22 14:33:00 +0200

    gofmt

--- cmd/ifx.go
+++ cmd/ifx.go
@@ -1,14 +1,21 @@
+// Copyright 2014 Gyepi Sam. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package main
 
 import (
-  "redux"
+	"fmt"
+	"os"
+
+	"redux"
 )
 
 var cmdIfCreate = &Command{
-   Run: runIfCreate,
+	Run:       runIfCreate,
 	UsageLine: "redux ifcreate [TARGET...]",
-	LinkName: "redo-ifcreate",
-	Short: "Create dependency on non-existence of targets.",
+	LinkName:  "redo-ifcreate",
+	Short:     "Creates dependency on non-existence of targets.",
 	Long: `
 The ifcreate command creates a dependency on the non-existence of the target files.
 The current file will be invalidated if the target comes into existence.
@@ -16,17 +23,17 @@ If the target exists, the command returns an error.
 `,
 }
 
-func runIfCreate(args[]string) {
-	redux.RedoIfX(func(file *redux.File, dependent *redux.File) error {
+func runIfCreate(args []string) error {
+	return redoIfX(args, func(file *redux.File, dependent *redux.File) error {
 		return file.RedoIfCreate(dependent)
 	})
 }
 
 var cmdIfChange = &Command{
-   Run: runIfChange,
-	UsageLine: "redux ifcreate [TARGET...]",
-	LinkName: "redo-ifchange",
-	Short: "Create dependency on targets and ensure that targets are up to date.",
+	Run:       runIfChange,
+	UsageLine: "redux ifchange [TARGET...]",
+	LinkName:  "redo-ifchange",
+	Short:     "Creates dependency on targets and ensure that targets are up to date.",
 	Long: `
 The ifchange command creates a dependency on the target files and ensures that
 the target files are up to date, calling the redo command, if necessary.
@@ -35,8 +42,37 @@ The current file will be invalidated if a target is rebuilt.
 `,
 }
 
-func runIfChange(args[]string) {
-	redux.RedoIfX(func(file *redux.File, dependent *redux.File) error {
+func runIfChange(args []string) error {
+	return redoIfX(args, func(file *redux.File, dependent *redux.File) error {
 		return file.RedoIfChange(dependent)
 	})
+}
+
+func redoIfX(args []string, fn func(*redux.File, *redux.File) error) error {
+
+	dependentPath := os.Getenv(redux.REDO_PARENT_ENV_NAME)
+	if len(dependentPath) == 0 {
+		return fmt.Errorf("Missing env variable %s", redux.REDO_PARENT_ENV_NAME)
+	}
+
+	wd, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+
+	// The action is triggered by dependent.
+	dependent, err := redux.NewFile(wd, dependentPath)
+	if err != nil {
+		return err
+	}
+
+	for _, path := range args {
+		if file, err := redux.NewFile(wd, path); err != nil {
+			return err
+		} else if err := fn(file, dependent); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
