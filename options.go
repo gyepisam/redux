1faf8e6a471e8b6d8b8983fda6d8253021173c58
Gyepi Sam <self@gyepi.com>
2014-01-22 14:18:27 +0200

    simplify options, move RedoIfX into cmd

--- options.go
+++ options.go
@@ -5,51 +5,14 @@
 package redux
 
 import (
-	"flag"
 	"os"
 )
 
-// Options set by main()
+// Options default to env values, to be overriden by main() if necessary.
 var (
-	Verbosity int
-	Debug     bool
-	ShellArgs string
+	Verbosity = len(os.Getenv("REDO_VERBOSE"))
+	Debug     = len(os.Getenv("REDO_DEBUG")) > 0
+	ShellArgs = os.Getenv("REDO_SHELL_ARGS")
 )
 
-func init() {
-	Verbosity = len(os.Getenv("REDO_VERBOSE"))
-	Debug = len(os.Getenv("REDO_DEBUG")) > 0
-	ShellArgs = os.Getenv("REDO_SHELL_ARGS")
-}
-
 func Verbose() bool { return Verbosity > 0 }
-
-// RedoIfX abstracts functionality common to redo-ifchange and redo-ifcreate
-func RedoIfX(fn func(*File, *File) error) error {
-
-	dependentPath := os.Getenv(REDO_PARENT_ENV_NAME)
-	if len(dependentPath) == 0 {
-		Fatal("Missing env variable %s", REDO_PARENT_ENV_NAME)
-	}
-
-	wd, err := os.Getwd()
-	if err != nil {
-	  return err
-	}
-
-	// The action is triggered by dependent.
-	dependent, err := NewFile(wd, dependentPath)
-	if err != nil {
-		FatalErr(err)
-	}
-
-	for _, path := range flag.Args() {
-		if file, err := NewFile(wd, path); err != nil {
-			FatalErr(err)
-		} else if err := fn(file, dependent); err != nil {
-			FatalErr(err)
-		}
-	}
-
-	return nil
-}
