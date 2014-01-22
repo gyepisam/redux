5f6bd4bfa7a96300addf967755226f8a5035d475
Gyepi Sam <self@gyepi.com>
2014-01-22 14:33:00 +0200

    fmt

--- nulldb.go
+++ nulldb.go
@@ -11,14 +11,14 @@ type NullDb struct {
 
 // Open requires a project root argument
 func NullDbOpen(ignored string) (DB, error) {
-  return &NullDb{}, nil
+	return &NullDb{}, nil
 }
 
 func (db *NullDb) IsNull() bool { return true }
 
 // Put stores value under key
 func (db *NullDb) Put(key string, value []byte) error {
-  return nil
+	return nil
 }
 
 // Get returns the value stored under key and a boolean indicating
@@ -30,24 +30,24 @@ func (db *NullDb) Get(key string) ([]byte, bool, error) {
 // Delete removes the value stored under key.
 // If key does not exist, the operation is a noop.
 func (db *NullDb) Delete(key string) error {
-  return nil
+	return nil
 }
 
 // GetKeys returns a list of keys which have the specified key as a prefix.
 func (db *NullDb) GetKeys(prefix string) ([]string, error) {
-  return []string{}, nil
+	return []string{}, nil
 }
 
 // GetValues returns a list of values whose keys matching the specified key prefix.
 func (db *NullDb) GetValues(prefix string) ([][]byte, error) {
-  return [][]byte{}, nil
+	return [][]byte{}, nil
 }
 
 // GetRecords returns a list of records (keys and data) matchign the specified key prefix.
 func (db *NullDb) GetRecords(prefix string) ([]Record, error) {
-  return []Record{}, nil
+	return []Record{}, nil
 }
 
 func (db *NullDb) Close() error {
-  return nil
+	return nil
 }
