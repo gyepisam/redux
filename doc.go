530b455e8585ac76d002b433cf659e7fbf811c30
Gyepi Sam <self@gyepi.com>
2014-01-27 08:02:58 +0200

    New file for better godoc documentation

---
+++ doc.go
@@ -0,0 +1,18 @@
+/*
+
+redux is a library for redux, a top down software build tool
+which implements and extends the redo concept somewhat sparsely
+described by [DJ Bernstein](http://cr.yp.to/redo.html).
+
+To install it, run
+
+    $ go get github.com/gyepisam/redux/redux
+
+The command
+
+	$ redux help
+
+provides built-in help, which is augmented by the html, man
+pages and the README file.
+*/
+package redux
